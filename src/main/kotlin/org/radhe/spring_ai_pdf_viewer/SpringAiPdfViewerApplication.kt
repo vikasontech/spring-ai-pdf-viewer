888cf271f79ca94798e3b86e4b1c01229a153a95
Vikas Verma <vikas.on@gmail.com>
2026-07-17 14:39:06 +0700

    initial version

---
+++ src/main/kotlin/org/radhe/spring_ai_pdf_viewer/SpringAiPdfViewerApplication.kt
@@ -0,0 +1,11 @@
+package org.radhe.spring_ai_pdf_viewer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiPdfViewerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAiPdfViewerApplication>(*args)
+}
---
+++ src/test/kotlin/org/radhe/spring_ai_pdf_viewer/SpringAiPdfViewerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.radhe.spring_ai_pdf_viewer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiPdfViewerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
